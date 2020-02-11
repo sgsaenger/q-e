abb68bc81ffa65fa80d317aa2d0baba53d3e6fd9
Daniele Cesarini <d.cesarini@cineca.it>
2020-02-11 13:50:13 +0100

    Replaced dgamma function with gamma because dgamma is only a GNU specific builtin

--- upftools/HGH2QE/hgh2qe.f90
+++ upftools/HGH2QE/hgh2qe.f90
@@ -302,7 +302,7 @@ subroutine vnonloc (mesh,nkbeta,ipspcod,lpx,psppar,r_l,rr,pot_Dij,lproj,nproj)
       jproj=nfproj
    end if
    lq=lproj(iproj)
-   rnrm=1.d0/dsqrt(.5d0*dgamma(lq+1.5d0+2.d0*jproj-2.d0)*  &
+   rnrm=1.d0/dsqrt(.5d0*gamma(lq+1.5d0+2.d0*jproj-2.d0)*  &
          r_l(lq+1)**(2*lq+3+4*(jproj-1)))
    do imesh=1,mesh
        r=rr(imesh)
